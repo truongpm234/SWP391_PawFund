c3a1fed187f3b4b6f3235b6ef5f81886248ac6ee
truongpm234 <167696397+truongpm234@users.noreply.github.com>
2024-10-07 22:26:56 +0700

    add foreignkey for Pet

--- MyWebApp1/obj/Debug/net8.0/MyWebApp1.AssemblyInfo.cs
+++ MyWebApp1/obj/Debug/net8.0/MyWebApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f25ee068d39019d867a1d58f06a8937d1a5548c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e7f5744d3051c7dfbcac68aff1372dc31727796")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
