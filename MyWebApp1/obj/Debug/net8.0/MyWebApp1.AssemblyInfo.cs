1eafbd631371be4215ee0b64feede9bace95d43f
Dathtse161884 <dathtse161884@fpt.edu.vn>
2024-10-11 14:12:38 +0700

    pull and merge main

--- MyWebApp1/obj/Debug/net8.0/MyWebApp1.AssemblyInfo.cs
+++ MyWebApp1/obj/Debug/net8.0/MyWebApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7fde8e679a910ee2c80e7ef5d75966a3535fa72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a608d7bc9fb500aa99076670f249d79a2d13f5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
