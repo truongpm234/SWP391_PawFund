b7df39067889b14bc6b51c7c6bba11d09cc03eb8
Quian2607 <dathtse161884@fpt.edu.vn>
2024-10-27 14:40:11 +0700

    merger

--- MyWebApp1/obj/Debug/net8.0/MyWebApp1.AssemblyInfo.cs
+++ MyWebApp1/obj/Debug/net8.0/MyWebApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f151be01d624486a902f80927958f54fab6a98c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fe1cb89d2848fac730eb90ad41a0dcadfa54e17")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
