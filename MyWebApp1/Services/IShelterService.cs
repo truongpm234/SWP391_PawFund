cfc411f9acc392ce15609c5b710fd96b6b7ae5c9
Quian2607 <dathtse161884@fpt.edu.vn>
2024-11-03 10:55:06 +0700

    thêm create, update, deleete shelter

--- MyWebApp1/Services/IShelterService.cs
+++ MyWebApp1/Services/IShelterService.cs
@@ -1,4 +1,5 @@
 ﻿using MyWebApp1.DTO;
+using MyWebApp1.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -8,5 +9,8 @@ namespace MyWebApp1.Services
     {
         Task<IEnumerable<ShelterWithPetsDTO>> GetAllSheltersWithPetsAsync();
         Task<ShelterWithPetsDTO> GetShelterWithPetsByIdAsync(int shelterId);
+        Task<Shelter> CreateShelterAsync(ShelterCreateDTO shelterDto);
+        Task<bool> UpdateShelterAsync(int shelterId, ShelterCreateDTO shelterDto);
+        Task<bool> DeleteShelterAsync(int shelterId);
     }
 }
